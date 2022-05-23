99416e4c7db536f2ca99523e97c79566ce8b9b9c
Ty-Irving <ty.irving1@ucalgary.ca>
2022-05-23 12:25:49 -0600

    Changed Library
    using pandas in order to fetch table content

--- scraper.py
+++ scraper.py
@@ -1,15 +1,14 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
+import pandas as pd
 import time
 
 class Scraper():
     def scrapedata(self, code):
         url = f'https://uk.flightaware.com/live/airport/{code}'
-        s = HTMLSession()
-        r = s.get(url)
-        soup = BeautifulSoup(r.html.html, 'html.parser')
-        table = soup.find('div', {'class': 'large-2 medium-4 small-4 columns bottomShadowBackground thinGrayBorder no-padding-ad'})
-        print(table.prettify)
+        tables = pd.read_html(url)
+        print(tables[0])
+        print(tables[1])
         
 airport = Scraper()
 airport.scrapedata('cyyc')
